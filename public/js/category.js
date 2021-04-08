28d2eb1f4d974085efef79a3ac2416b1dc39dba6
sinhnguyen777 <71301171+sinhnguyen777@users.noreply.github.com>
2021-04-08 23:29:46 +0700

    project js nang cao

---
+++ public/js/category.js
@@ -0,0 +1,23 @@
+// var url = 'http://localhost:3000/category'
+
+// fetch(url, {
+//     method: 'GET'
+// })
+// .then((response) => {
+//     return response.json()
+// })
+// .then((data) => {
+//     // console.log(data);
+//     data.forEach((index, item) => {
+//         // console.log(index);
+//         document.querySelector('#listCategory').innerHTML += `
+//         <li class="befor-check">
+//             <div class="form-check pt-2 pb-2">
+//                 <label class="form-check-label">
+//                     <a href="">${index.name}</a>
+//                 </label>
+//             </div>
+//         </li>
+//         `
+//     });
+// })
