5f5ab3eed414b62a617f5cddca18847876597285
sinhnguyen777 <71301171+sinhnguyen777@users.noreply.github.com>
2021-04-15 20:01:40 +0700

    delete cart

--- public/js/cart.js
+++ public/js/cart.js
@@ -1,65 +1,3 @@
-var carts = document.querySelectorAll(".btn-add");
-for (let i = 0; i < carts.length; i++) {
-    carts[i].addEventListener("click", () => {
-        cartNumbers(arr[i]);
-        tolalCost(arr[i])
-    });
-}
-
-function onLoadCartNumbers() {
-    var productNumbers = localStorage.getItem("cartNumbers");
-    if (productNumbers) {
-        document.querySelector("#itemCartSpan").textContent = productNumbers;
-    }
-}
-
-function cartNumbers(product) {
-    var productNumbers = localStorage.getItem("cartNumbers");
-
-    productNumbers = parseInt(productNumbers);
-    if (productNumbers) {
-        localStorage.setItem("cartNumbers", productNumbers + 1);
-        document.querySelector("#itemCartSpan").textContent = productNumbers + 1;
-    } else {
-        localStorage.setItem("cartNumbers", 1);
-        document.querySelector("#itemCartSpan").textContent = 1;
-    }
-    setItem(product);
-}
-
-function setItem(product) {
-    var cartItem = localStorage.getItem("product");
-    cartItem = JSON.parse(cartItem)
-
-    if (cartItem != null) {
-        if (cartItem[product.name] == undefined) {
-            cartItem = {
-                ...cartItem,
-                [product.name]: product
-            }
-        }
-        cartItem[product.name].quantity += 1;
-    } else {
-        product.quantity = 1
-        cartItem = {
-            [product.name]: product
-        }
-    }
-    localStorage.setItem("product", JSON.stringify(cartItem));
-}
-
-function tolalCost(product) {
-    let cartCost = localStorage.getItem('totalCost');
-    if (cartCost != null) {
-        cartCost = parseInt(cartCost)
-        localStorage.setItem('totalCost', cartCost + product.price)
-    } else {
-        localStorage.setItem('totalCost', product.price)
-    }
-}
-
-onLoadCartNumbers();
-
 function displayCart() {
     let cartItems = localStorage.getItem('product')
     cartItems = JSON.parse(cartItems)
@@ -105,9 +43,9 @@ function displayCart() {
         `
     }
     if (cartItems && productToltals) {
-        
+
         var quantityCart = 0
-        var tong = cartItems.reduce(function(accumulator, currentValue){
+        var tong = cartItems.reduce(function (accumulator, currentValue) {
             var sl = accumulator + currentValue.quantity
             return sl
         }, quantityCart)
@@ -163,17 +101,24 @@ function displayCart() {
 
 displayCart();
 
-function deleteCart(id){
+function deleteCart(id) {
     let getCartStorage = JSON.parse(localStorage.getItem('product'))
-    let getCartNumbers = JSON.parse(localStorage.getItem('cartNumbers'))
-    let number = getCartNumbers - 1
-
     getCartStorage = getCartStorage.filter((value, index) => value.id != id)
     localStorage.setItem('product', JSON.stringify(getCartStorage))
+    let tt = 0
+    getCartStorage.forEach((item) => {
+        if (item.quantity > 0) {
+            var totalCart = item.price * item.quantity
+            tt += totalCart
+        }
+    });
+    localStorage.setItem('totalCost', JSON.stringify(tt))
+
+    //so luong trong gio hang
+    let getCartNumbers = JSON.parse(localStorage.getItem('cartNumbers'))
+    let number = getCartNumbers - 1
     localStorage.setItem('cartNumbers', JSON.stringify(number))
     document.querySelector("#itemCartSpan").textContent = localStorage.getItem('cartNumbers')
     displayCart()
-    // showCartLocal()
 }
-
 
